e3b5d9a8717e1220fdf03264d003d2b1fc6bc173
dilish <dilish2691@gmail.com>
2021-07-13 10:57:16 +0000

    added git igonre file

--- mysqlDBbackup.sh
+++ mysqlDBbackup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-mysqldump -h db_host.rds.aws.com -u dbuser -p welcome1 db_flipkart > /db-backup/db-flipkart.sql
+v_dbName=$1
+v_username=$(cat cred | grep username | awk -F "=" '{print $2}')
+v_password=$(cat cred | grep password | awk -F "=" '{print $2}')
+v_db_host=dbhost.rds.aws.com
+
+echo "mysqldump -h $v_db_host -u $v_username -p $v_password $v_dbName > /db-backup/$v_dbName.sql"
